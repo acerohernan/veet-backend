cbba0c6c68b25261d171233ce2af6b0de80c5d4f
Hernan Acero <contacto.acero.hernan@gmail.com>
2025-03-11 18:24:32 -0500

    feat: se agregó ruta para el status health check

---
+++ Veet.Api/Controllers/RootController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Veet.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet("/status")]
+        public IActionResult GetStatus()
+        {
+            return Ok("OK");
+        }
+    }
+}
